c0807de4da19cdf4b4b1d2e2ce99066e628fb1ae
Zack <59828681+moeenkhurram@users.noreply.github.com>
2021-05-16 07:38:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-model = load_model('deployment_28042020')
+model = load_model('model')
 cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
 @app.route('/')
